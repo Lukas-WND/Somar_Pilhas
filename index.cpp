d3f0959e91eaf36582a6656bf15d76c0e7937da4
Lukas Wendel <lukas.wrks@gmail.com>
2023-11-16 15:05:53 -0400

    new methods of operations in stacks

--- index.cpp
+++ index.cpp
@@ -9,7 +9,7 @@ using namespace std;
 typedef struct Pilha {
     int num;
     Pilha *acima, *abaixo;
-};
+} Pilha;
 
 bool isEmpty(Pilha *topo){
     if(topo == nullptr){
@@ -40,6 +40,37 @@ void push(Pilha **topo, int num){
     }
 }
 
+void pop(Pilha **topo){
+    if(!isEmpty(*topo)){
+        Pilha *temp = (*topo);
+        (*topo) = (*topo)->abaixo;
+
+        delete(temp);
+    } else {
+        (*topo) = nullptr;
+    }
+}
+
+Pilha *topEl(Pilha *pilha){
+    if(pilha == nullptr){
+        return nullptr;
+    } else {
+        return pilha;
+    }
+}
+
+void clear(Pilha **pilha){
+    if(!isEmpty(*pilha)){
+        Pilha *aux = (*pilha);
+
+        while(aux != nullptr){
+            (*pilha) = (*pilha)->abaixo;
+            delete(aux);
+            aux = (*pilha);
+        }
+    }
+}
+
 void inserirNumero(Pilha **pilha, int num){
     string numStr = to_string(num);
 
@@ -47,12 +78,41 @@ void inserirNumero(Pilha **pilha, int num){
         int digInt = digito - '0';
 
         push(pilha, digInt);
-        cout << "Dig: " << digInt << endl;
     }
 }
 
+Pilha *somar(Pilha **num1, Pilha **num2) {
+    int carry = 0;
+    Pilha *resultado = new Pilha();
+
+    while (!isEmpty(*num1) || !isEmpty(*num2)) {
+        int soma = carry;
+
+        if (!isEmpty(*num1)) {
+            soma += topEl(*num1)->num;
+            pop(num1);
+        }
+
+        if (!isEmpty(*num2)) {
+            soma += topEl(*num2)->num;
+            pop(num2);
+        }
+
+        int digito = soma % 10;
+        carry = soma / 10;
+
+        push(&resultado, digito);
+    }
+
+    if(carry != 0){
+        push(&resultado, carry);
+    }
+
+    return resultado;
+}
+
 int main(){
-    Pilha *pilha1, *pilha2;
+    Pilha *pilha1 = nullptr, *pilha2 = nullptr;
     int num1 = 0, num2 = 0;
 
     cout << "SOMAR DOIS NUMEROS" << endl;
@@ -67,4 +127,16 @@ int main(){
     cin.ignore();
     inserirNumero(&pilha2, num2);
 
+    Pilha *resultado = somar(&pilha1, &pilha2);
+
+    cout << "Resultado da soma: ";
+
+    while(!isEmpty(resultado)){
+        cout << topEl(resultado)->num;
+        pop(&resultado);
+    }
+
+    cout << endl;
+
+    return 0;
 }
